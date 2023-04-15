2b1c140957955f4c45fca9767b7fe77d0e0b5a9c
joaomalfatti <joao.cursos1210@gmail.com>
2023-04-15 16:37:04 -0400

    Nessa versão foi adicionado a opção de Porcentagem no código

--- calculadora.py
+++ calculadora.py
@@ -17,6 +17,14 @@ def divisao(a, b):
         return None
     else:
         return a / b
+    
+#Funão de porcentagem
+def porcentagem(a, b):
+    if b == 0:
+        print("Erro: porcentagem por zero!")
+        return None
+    else:
+        return (a * b) / 100
 
 # Função principal
 def calculadora():
@@ -26,14 +34,15 @@ def calculadora():
         print("2. Subtração")
         print("3. Multiplicação")
         print("4. Divisão")
-        print("5. Sair")
-        opcao = input("Digite o número da operação que você deseja (1/2/3/4/5): ")
+        print("5. Porcentagem")
+        print("6. Sair")
+        opcao = input("Digite o número da operação que você deseja (1/2/3/4/5/6): ")
 
-        if opcao == '5':
+        if opcao == '6':
             print("Encerrando a calculadora...")
             break
 
-        if opcao not in ['1', '2', '3', '4']:
+        if opcao not in ['1', '2', '3', '4', '5']:
             print("Opção inválida. Tente novamente.")
             continue
 
@@ -42,17 +51,21 @@ def calculadora():
 
         if opcao == '1':
             resultado = soma(num1, num2)
-            print("Resultado: ", resultado)
+            print("Resultado é: ", resultado)
         elif opcao == '2':
             resultado = subtracao(num1, num2)
-            print("Resultado: ", resultado)
+            print("Resultado é: ", resultado)
         elif opcao == '3':
             resultado = multiplicacao(num1, num2)
-            print("Resultado: ", resultado)
+            print("Resultado é: ", resultado)
         elif opcao == '4':
             resultado = divisao(num1, num2)
             if resultado is not None:
-                print("Resultado: ", resultado)
+                print("Resultado é: ", resultado)
+        elif opcao == '5':
+            resultado = porcentagem(num1, num2)
+            if resultado is not None:
+                print("Resultado é: ", resultado)
 
 # Chamada da função principal
 calculadora()
